dc37fe971a91737e23d8493f11c7424f46590113
Sergey Andryukhin <sorgoz@yandex.com>
2013-01-21 15:47:58 +0200

    Updated version to 1.10

--- wot.js
+++ wot.js
@@ -1,6 +1,6 @@
 /*
 	wot.js
-	Copyright © 2009 - 2012  WOT Services Oy <info@mywot.com>
+	Copyright © 2009 - 2013  WOT Services Oy <info@mywot.com>
 
 	This file is part of WOT.
 
@@ -19,7 +19,7 @@
 */
 
 var wot = {
-	version: 20120718,
+	version: 20130121,
 	platform: "opera",
 	language: "en",		/* default */
 	debug: false,
